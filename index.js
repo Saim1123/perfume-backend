e3ab5d5c5f6beaf92b9e5eb233e29d7b69721f76
SaimTahir <127545146+Saim1123@users.noreply.github.com>
2024-09-09 12:45:55 +0500

    bug fixed.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/product", productRoutes);
+app.use("/api/v1/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
