5a155422de4f7fec6ff24fcaee1486d1b69b1a72
SaimTahir <127545146+Saim1123@users.noreply.github.com>
2024-09-09 12:45:25 +0500

    Add new fields and validations to product schema

--- models/Product.js
+++ models/Product.js
@@ -2,14 +2,18 @@ import { Schema, model } from "mongoose";
 
 const productSchema = new Schema(
   {
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: String, required: true },
+    name: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    price: { type: Number, required: true },
     brand: { type: String, required: true },
     stock: { type: Number, required: true },
     images: [{ type: String, required: true }],
     rating: { type: Number, default: 0 },
-    intensity: { type: String, required: true },
+    intensity: { type: String, required: true, enum: ["light", "medium", "strong"] },
+    size: { type: String, required: true },
+    gender: { type: String, enum: ["male", "female", "unisex"], required: true },
+    fragranceFamily: { type: String, required: true },
+    reviews: [{ type: String }],
   },
   { timestamps: true },
 );
